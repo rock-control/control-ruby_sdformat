7866c61eee75f2d9da5b9a29730fe71a66e45611
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-03-20 08:50:51 -0300

    Initial commit

---
+++ lib/sdf.rb
@@ -0,0 +1,11 @@
+require 'sdf/sdf'
+require 'sdf/base'
+
+# The toplevel namespace for sdf
+#
+# You should describe the basic idea about sdf here
+require 'utilrb/logger'
+module Sdf
+    extend Logger::Root('Sdf', Logger::WARN)
+end
+
