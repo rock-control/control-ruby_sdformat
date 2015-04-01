893ee8c6b3eb0aaba58e5441afded271495a3ddd
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-04-01 16:46:30 -0300

    test: update suite.rb

--- test/suite.rb
+++ test/suite.rb
@@ -1,3 +1,11 @@
 # Require all your test files here. Always prepend ./ and use the relative path
 # to the Ruby library root
 require './test/test_xml'
+require './test/test_axis_limit'
+require './test/test_axis'
+require './test/test_element'
+require './test/test_joint'
+require './test/test_link'
+require './test/test_model'
+require './test/test_root'
+require './test/test_eigen_conversions'
