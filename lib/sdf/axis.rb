26a991c907b0e54ca958c4bcc2b31421c9775a8b
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-04-02 15:11:08 -0300

    add support for Axis#use_parent_model_frame?

--- lib/sdf/axis.rb
+++ lib/sdf/axis.rb
@@ -4,6 +4,14 @@ module SDF
             EigenConversions.vector3_to_eigen(xml.elements['xyz'])
         end
 
+        def use_parent_model_frame?
+            if flag = xml.elements['use_parent_model_frame']
+                flag.text == '1'
+            else
+                false
+            end
+        end
+
         def limit
             child_by_name('limit', AxisLimit, false)
         end
