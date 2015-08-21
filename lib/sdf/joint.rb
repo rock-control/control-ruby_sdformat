a638973affa2c5fdec25cd8ff467583b19c0e240
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-08-21 13:33:55 -0300

    add minimal support for sensors

--- lib/sdf/joint.rb
+++ lib/sdf/joint.rb
@@ -11,6 +11,16 @@ module SDF
             end
         end
 
+        # Enumerates this joint's sensors
+        #
+        # @yieldparam [Sensor] sensor
+        def each_sensor
+            return enum_for(__method__) if !block_given?
+            xml.elements.each('sensor') do |element|
+                yield(Sensor.new(element, self))
+            end
+        end
+
         # The model's pose w.r.t. its parent
         #
         # @return [Array<Float>]
--- lib/sdf/link.rb
+++ lib/sdf/link.rb
@@ -2,6 +2,16 @@ module SDF
     class Link < Element
         xml_tag_name 'link'
 
+        # Enumerates this link's sensors
+        #
+        # @yieldparam [Sensor] sensor
+        def each_sensor
+            return enum_for(__method__) if !block_given?
+            xml.elements.each('sensor') do |element|
+                yield(Sensor.new(element, self))
+            end
+        end
+
         # The model's pose w.r.t. its parent
         #
         # @return [Array<Float>]
