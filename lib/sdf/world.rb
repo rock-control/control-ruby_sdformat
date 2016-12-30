b317a4505855d4d90c49352cfc4bd152bffb8cb7
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2016-12-30 14:44:50 -0200

    add World.empty to create an empty world

--- lib/sdf/world.rb
+++ lib/sdf/world.rb
@@ -1,5 +1,21 @@
 module SDF
     class World < Element
+        # Return an empty world
+        def self.empty(name: 'world', version: nil)
+            if version && !version.respond_to?(:to_str)
+                version = SDF.numeric_version_to_string(version)
+            end
+
+            xml = REXML::Document.new
+            root = xml.add_element 'sdf'
+            if version
+                root.attributes['version'] = version
+            end
+            world = root.add_element 'world'
+            world.attributes['name'] = name
+            Root.new(xml)
+        end
+
         xml_tag_name 'world'
 
         # Enumerates the models from this world
